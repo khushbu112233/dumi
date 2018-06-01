df273dfe07fe2929fe3757f29d0f75f966e8ce34
DESKTOP-VQ5PP80\khushbu987 <khushbu.amplearch@gmail.com>
2018-06-01 12:35:28 +0530

    +data binding+

--- Dumi/app/src/main/java/com/ample/dumi/Activity/LoginActivity.java
+++ Dumi/app/src/main/java/com/ample/dumi/Activity/LoginActivity.java
@@ -1,15 +1,22 @@
 package com.ample.dumi.Activity;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.ample.dumi.R;
+import com.ample.dumi.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding activityLoginBinding;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+       activityLoginBinding = DataBindingUtil.setContentView(this,R.layout.activity_login);
+       
     }
 }
---
+++ Dumi/app/src/main/java/com/ample/dumi/Activity/SignUpActivity.java
@@ -0,0 +1,19 @@
+package com.ample.dumi.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ample.dumi.R;
+
+/**
+ * Created by khushbu987 on 6/1/2018.
+ */
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
