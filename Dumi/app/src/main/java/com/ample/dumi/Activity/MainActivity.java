0307bcf257c44879d79374e38bc3889660fc7a78
Kajal <kajal.patadia@ample-arch.com>
2018-06-01 17:34:01 +0530

    +loan layout

--- Dumi/app/src/main/java/com/ample/dumi/Activity/MainActivity.java
+++ Dumi/app/src/main/java/com/ample/dumi/Activity/MainActivity.java
@@ -1,15 +1,23 @@
 package com.ample.dumi.Activity;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ample.dumi.Fragments.LoanFragment;
 import com.ample.dumi.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Fragment fragment = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragment = new LoanFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container_wrapper, fragment)
+                .addToBackStack(null).commit();
+
     }
 }
