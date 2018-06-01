c61b016eb5eb9592fa971568256827be9dee4f12
DESKTOP-VQ5PP80\khushbu987 <khushbu.amplearch@gmail.com>
2018-06-01 13:24:15 +0530

    +signup

--- Dumi/app/src/main/java/com/ample/dumi/Activity/LoginActivity.java
+++ Dumi/app/src/main/java/com/ample/dumi/Activity/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.ample.dumi.Activity;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.ample.dumi.R;
 import com.ample.dumi.databinding.ActivityLoginBinding;
@@ -16,7 +19,14 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       activityLoginBinding = DataBindingUtil.setContentView(this,R.layout.activity_login);
+        activityLoginBinding = DataBindingUtil.setContentView(this,R.layout.activity_login);
 
+        activityLoginBinding.btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
