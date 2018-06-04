493903afc8c8d80d24df2912b9ffdd094aa47f66
DESKTOP-VQ5PP80\khushbu987 <khushbu.amplearch@gmail.com>
2018-06-04 14:38:47 +0530

    +dummy transfer+

---
+++ Dumi/app/src/main/java/com/ample/dumi/Activity/TransfertoDummyActivity.java
@@ -0,0 +1,15 @@
+package com.ample.dumi.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ample.dumi.R;
+
+public class TransfertoDummyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transferto_dummy);
+    }
+}
