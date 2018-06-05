f3b1f52d492e99441dfe0e1b2c1e2e97aeed67d3
Kajal <kajal.patadia@ample-arch.com>
2018-06-05 18:12:17 +0530

    +navigation +design

--- Dumi/app/src/main/java/com/ample/dumi/Activity/TransferToYourBankActivity.java
+++ Dumi/app/src/main/java/com/ample/dumi/Activity/TransferToYourBankActivity.java
@@ -1,11 +1,13 @@
 package com.ample.dumi.Activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.SeekBar;
 
 import com.ample.dumi.R;
 import com.ample.dumi.databinding.ActivityTransferToYourBankBinding;
@@ -25,6 +27,23 @@ ActivityTransferToYourBankBinding activityTransferToYourBankBinding;
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        activityTransferToYourBankBinding.seekTransToYourBank.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Intent i = new Intent(TransferToYourBankActivity.this,AddPayeeTransferActivity.class);
+                startActivity(i);
             }
         });
     }
