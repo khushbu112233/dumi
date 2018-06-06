5c5bff1d933704a5851f98e3026d87baf9034de6
DESKTOP-VQ5PP80\khushbu987 <khushbu.amplearch@gmail.com>
2018-06-06 13:12:11 +0530

    +changes+

--- Dumi/app/src/main/java/com/ample/dumi/Activity/AddPayeeDumiTransferActivity.java
+++ Dumi/app/src/main/java/com/ample/dumi/Activity/AddPayeeDumiTransferActivity.java
@@ -1,9 +1,11 @@
 package com.ample.dumi.Activity;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
 import com.ample.dumi.R;
 import com.ample.dumi.databinding.ActivityAddPayeeDumiTransferBinding;
@@ -22,5 +24,29 @@ public class AddPayeeDumiTransferActivity extends AppCompatActivity {
                 finish();
             }
         });
+        activityAddPayeeDumiTransferBinding.seekbarDumi.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+
+                activityAddPayeeDumiTransferBinding.txtSeek.setTextColor(getResources().getColor(R.color.white));
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        activityAddPayeeDumiTransferBinding.seekbarDumi.setProgress(0);
+        activityAddPayeeDumiTransferBinding.txtSeek.setTextColor(getResources().getColor(R.color.black));
     }
 }
