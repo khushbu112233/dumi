9c51c782c05781b8d601b9c5a623eaf904f84ac3
DESKTOP-VQ5PP80\khushbu987 <khushbu.amplearch@gmail.com>
2018-06-06 13:41:17 +0530

    +make color changes in transfer+

--- Dumi/app/src/main/java/com/ample/dumi/Activity/TransferToYourBankActivity.java
+++ Dumi/app/src/main/java/com/ample/dumi/Activity/TransferToYourBankActivity.java
@@ -50,5 +50,6 @@ public class TransferToYourBankActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         activityTransferToYourBankBinding.seekTransToYourBank.setProgress(0);
+
     }
 }
