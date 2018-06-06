12ccba8883ed7a9fb008d50522c81ec15a3b2c55
DESKTOP-VQ5PP80\khushbu987 <khushbu.amplearch@gmail.com>
2018-06-06 12:07:22 +0530

    + transfer+

--- Dumi/app/src/main/java/com/ample/dumi/Activity/TransfertoAnotherBankActivity.java
+++ Dumi/app/src/main/java/com/ample/dumi/Activity/TransfertoAnotherBankActivity.java
@@ -6,6 +6,7 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.SeekBar;
 
@@ -19,11 +20,8 @@ public class TransfertoAnotherBankActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         activityTransfertoAnotherBankBinding = DataBindingUtil.setContentView(this,R.layout.activity_transferto_another_bank);
-        View view = this.getCurrentFocus();
-        if (view != null) {
-            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        }
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        activityTransfertoAnotherBankBinding.includeActionbar1.txtActionBarTitle.setText(getResources().getString(R.string.transfer));
         activityTransfertoAnotherBankBinding.includeActionbar1.imgBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
