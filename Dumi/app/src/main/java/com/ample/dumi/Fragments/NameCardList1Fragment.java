e0faec1ed30c84c40f7a6c7147003a6d2bd9ca76
Kajal <kajal.patadia@ample-arch.com>
2018-06-07 12:06:03 +0530

    +tap nfc card click

--- Dumi/app/src/main/java/com/ample/dumi/Fragments/NameCardList1Fragment.java
+++ Dumi/app/src/main/java/com/ample/dumi/Fragments/NameCardList1Fragment.java
@@ -1,12 +1,20 @@
 package com.ample.dumi.Fragments;
 
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.ample.dumi.Adapter.GalleryAdapter;
 import com.ample.dumi.R;
@@ -60,6 +68,50 @@ public class NameCardList1Fragment extends Fragment {
         images.add(R.drawable.loan_bal);
         images.add(R.drawable.loan_bal);
 
+        fragmentNameCardList1Binding.lnrTapCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NfcManager manager = (NfcManager) getContext().getSystemService(Context.NFC_SERVICE);
+                NfcAdapter adapter = manager.getDefaultAdapter();
+                if (adapter != null && adapter.isEnabled()) {
+                    Toast.makeText(getContext(), "You can scan CircleOne NFC-Cards by holding them behind NFC sensor of your Android device. It could be either on top, middle or bottom.", Toast.LENGTH_LONG).show();
+                    //Yes NFC available
+                }
+                else if (adapter != null && !adapter.isEnabled()){
+
+
+                    AlertDialog.Builder builder1 = new AlertDialog.Builder(getContext());
+                    builder1.setMessage("Please ensure that NFC has been enabled under your phone settings. You will still be able to add contacts with the QR scan feature if your device isn't NFC capable.");
+                    builder1.setCancelable(true);
+
+                    builder1.setPositiveButton(
+                            "Settings",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    Toast.makeText(getContext(), "Please activate NFC and press Back to return to the application!", Toast.LENGTH_LONG).show();
+                                    startActivity(new Intent(Settings.ACTION_NFC_SETTINGS));
+                                    dialog.cancel();
+                                }
+                            });
+
+                    builder1.setNegativeButton(
+                            "Cancel",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+
+                }
+                else{
+                    Toast.makeText(getContext(), "Your device does not support NFC. You will still be able to add contacts with the QR scan feature if your device isn't NFC capable.", Toast.LENGTH_LONG).show();
+                    //Your device doesn't support NFC
+                }
+            }
+        });
         return view;
     }
 
