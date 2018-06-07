d04993807e3cbf631c9f9e8738209f03c075b777
DESKTOP-VQ5PP80\khushbu987 <khushbu.amplearch@gmail.com>
2018-06-07 16:21:14 +0530

    +add qr code+

--- Dumi/app/src/main/java/com/ample/dumi/Utils/Utility.java
+++ Dumi/app/src/main/java/com/ample/dumi/Utils/Utility.java
@@ -15,6 +15,7 @@ import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
@@ -38,12 +39,25 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
 
 public class Utility
 {
@@ -381,7 +395,47 @@ public class Utility
         inputStream.close();
         return result;
     }
+    public static String encrypt(String value, String key)
+            throws UnsupportedEncodingException, InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException
+    {
+        byte[] value_bytes = value.getBytes("UTF-8");
+        byte[] key_bytes = getKeyBytes(key);
+        return Base64.encodeToString(encrypt(value_bytes, key_bytes, key_bytes), 0);
+    }
+    private static byte[] getKeyBytes(String paramString)
+            throws UnsupportedEncodingException
+    {
+        byte[] arrayOfByte1 = new byte[16];
+        byte[] arrayOfByte2 = paramString.getBytes("UTF-8");
+        System.arraycopy(arrayOfByte2, 0, arrayOfByte1, 0, Math.min(arrayOfByte2.length, arrayOfByte1.length));
+        return arrayOfByte1;
+    }
+    public static byte[] encrypt(byte[] paramArrayOfByte1, byte[] paramArrayOfByte2, byte[] paramArrayOfByte3)
+            throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException
+    {
+        // setup AES cipher in CBC mode with PKCS #5 padding
+        Cipher localCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 
+        // encrypt
+        localCipher.init(1, new SecretKeySpec(paramArrayOfByte2, "AES"), new IvParameterSpec(paramArrayOfByte3));
+        return localCipher.doFinal(paramArrayOfByte1);
+    }
+    public static String decrypt(String value, String key)
+            throws GeneralSecurityException, IOException {
+        byte[] value_bytes = Base64.decode(value, 0);
+        byte[] key_bytes = getKeyBytes(key);
+        return new String(decrypt(value_bytes, key_bytes, key_bytes), "UTF-8");
+    }
+
+    public static byte[] decrypt(byte[] ArrayOfByte1, byte[] ArrayOfByte2, byte[] ArrayOfByte3)
+            throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+        // setup AES cipher in CBC mode with PKCS #5 padding
+        Cipher localCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+        // decrypt
+        localCipher.init(2, new SecretKeySpec(ArrayOfByte2, "AES"), new IvParameterSpec(ArrayOfByte3));
+        return localCipher.doFinal(ArrayOfByte1);
+    }
     public static String POST2(String url,JSONObject jsonObject)
     {
         InputStream inputStream = null;
