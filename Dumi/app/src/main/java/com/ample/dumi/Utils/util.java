30e39cf858804abc87c0cf3842e889fba9103b18
Kajal <kajal.patadia@ample-arch.com>
2018-06-07 12:58:50 +0530

    +nfc integration

--- Dumi/app/src/main/java/com/ample/dumi/Utils/util.java
+++ Dumi/app/src/main/java/com/ample/dumi/Utils/util.java
@@ -2,14 +2,35 @@ package com.ample.dumi.Utils;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AlertDialog;
+import android.util.Base64;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  * Created by khushbu987 on 6/5/2018.
  */
 
 public class util {
+    public static String secretKey = "1234567890234561";
+    public static final byte[] MIME_TEXT = "application/com.circle8.circleOne".getBytes();
+    public static final String BASE_URL = "http://circle8.asia:8082/Onet.svc/";
+    public static final String BASE_IMAGE_URL = "http://circle8.asia:8082/";
 
     public static  void showAlert(final Context context,String str)
     {
@@ -23,5 +44,37 @@ public class util {
                     }
                 });
         alertDialog.show();
+    }
+
+    public static String decrypt(String value, String key)
+            throws GeneralSecurityException, IOException {
+        byte[] value_bytes = Base64.decode(value, 0);
+        byte[] key_bytes = getKeyBytes(key);
+        return new String(decrypt(value_bytes, key_bytes, key_bytes), "UTF-8");
+    }
+
+    public static byte[] decrypt(byte[] ArrayOfByte1, byte[] ArrayOfByte2, byte[] ArrayOfByte3)
+            throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+        // setup AES cipher in CBC mode with PKCS #5 padding
+        Cipher localCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+        // decrypt
+        localCipher.init(2, new SecretKeySpec(ArrayOfByte2, "AES"), new IvParameterSpec(ArrayOfByte3));
+        return localCipher.doFinal(ArrayOfByte1);
+    }
+
+    private static byte[] getKeyBytes(String paramString)
+            throws UnsupportedEncodingException {
+        byte[] arrayOfByte1 = new byte[16];
+        byte[] arrayOfByte2 = paramString.getBytes("UTF-8");
+        System.arraycopy(arrayOfByte2, 0, arrayOfByte1, 0, Math.min(arrayOfByte2.length, arrayOfByte1.length));
+        return arrayOfByte1;
+    }
+
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
